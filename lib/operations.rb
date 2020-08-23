a05789cd585c9f29d401cd6f94da7dde7c8bc6c0
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-23 03:55:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,11 +2,11 @@ def unsafe?(speed)
   if speed > 60
     true
   elsif speed < 40
-    true
+    false
   else
   end
 end
 
 def not_safe?(speed)
-  speed < 40 || speed > 60 ? false
+  speed < 40 || speed > 60 ? true : false
 end
